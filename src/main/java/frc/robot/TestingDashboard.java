1614d3af4dd1aa906de95726027e5afe7e91245e
Caleb Holland <calebholland@Calebs-MacBook-Air.local>
2020-02-15 12:13:22 -0500

    adds command to drive robot forward for a certain amount of time

--- src/main/java/frc/robot/TestingDashboard.java
+++ src/main/java/frc/robot/TestingDashboard.java
@@ -145,8 +145,11 @@ public class TestingDashboard {
         SmartDashboard.putNumber("SpinnerMotorSpeed",0.2);
         SmartDashboard.putString("SpinnerTargetColor","Yellow");
         SmartDashboard.putString("SpinnerActualColor","Yellow");
+        
+        // Controlling amount of time to drive forward
+        SmartDashboard.putNumber("DriveForwardTime", 3);
     }
-
+ 
     public void updateDebugTab() {
 
     }
