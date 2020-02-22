f6b18d7465fb64e85dda985cd3c78087d51d8eb6
Thor Smith <thorsmith777@gmail.com>
2020-02-22 11:25:55 -0500

    updates turret left/right to be active while held
    This change adds back the turret left and right end
    action and removes the end condition (i.e., the command
    will run until cancelled). It also changes the button
    behavior so the command is active while held.

--- src/main/java/frc/robot/commands/Turret/TurretRight.java
+++ src/main/java/frc/robot/commands/Turret/TurretRight.java
@@ -44,11 +44,12 @@ public class TurretRight extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_turret.spinTurretMotor(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return false;
   }
 }
