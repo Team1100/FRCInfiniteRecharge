e89c227a657a6ace6e11b50eaa697a80aa1bccce
Thomas <awtpianka@gmail.com>
2020-02-23 16:04:39 -0500

    Paramaterize SpinShooter command

--- src/main/java/frc/robot/commands/Shooter/SpinShooter.java
+++ src/main/java/frc/robot/commands/Shooter/SpinShooter.java
@@ -17,16 +17,22 @@ public class SpinShooter extends CommandBase {
    * Creates a new SpinShooter.
    */
   Shooter m_shooter;
+  double m_topSpeed;
+  double m_botSpeed;
 
-  public SpinShooter() {
+  public SpinShooter(double topShooterSpeed, double bottomShooterSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(Shooter.getInstance());
     m_shooter = Shooter.getInstance();
+    m_topSpeed = topShooterSpeed;
+    m_botSpeed = bottomShooterSpeed;
   }
 
   public static void registerWithTestingDashboard() {
     Shooter shooter = Shooter.getInstance();
-    SpinShooter cmd = new SpinShooter();
+    double topSpeed = SmartDashboard.getNumber("TopShooterSpeed", 0.2);
+    double botSpeed = SmartDashboard.getNumber("BottomShooterSpeed", 0.2);
+    SpinShooter cmd = new SpinShooter(topSpeed, botSpeed);
     TestingDashboard.getInstance().registerCommand(shooter, "Basic", cmd);
   }
 
@@ -38,10 +44,8 @@ public class SpinShooter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double topSpeed = SmartDashboard.getNumber("TopShooterSpeed",0);
-    double botSpeed = SmartDashboard.getNumber("BottomShooterSpeed",0);
-    m_shooter.setTop(-topSpeed);
-    m_shooter.setBottom(-botSpeed);
+    m_shooter.setTop(m_topSpeed);
+    m_shooter.setBottom(m_botSpeed);
   }
 
   // Called once the command ends or is interrupted.
