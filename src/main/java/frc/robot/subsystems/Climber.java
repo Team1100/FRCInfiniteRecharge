d7ebc5596dcb552e80bae80fcb50189c0dd57d87
Thor Smith <thorsmith777@gmail.com>
2020-02-24 20:50:44 -0500

    updates climb subsystem to use VictorSPX motor controllers

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,27 +7,24 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.VictorSP;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 import frc.robot.TestingDashboard;
 
 public class Climber extends SubsystemBase {
+  
   private static Climber climber;
-
-
-  private VictorSP rightMotor;
-  private VictorSP leftMotor;
-
+  private VictorSPX rightMotor;
+  private VictorSPX leftMotor;
 
   /**
    * Creates a new Climber.
    */
   private Climber() {
-    
-    rightMotor = new VictorSP(RobotMap.CL_MOTOR_LEFT);
-    leftMotor = new  VictorSP(RobotMap.CL_MOTOR_RIGHT);
-
+    rightMotor = new VictorSPX(RobotMap.CL_MOTOR_LEFT);
+    leftMotor = new  VictorSPX(RobotMap.CL_MOTOR_RIGHT);
   }
 
   public static Climber getInstance() {
@@ -39,11 +36,11 @@ public class Climber extends SubsystemBase {
   }
 
   public void setRight(double speed) {
-    rightMotor.set(speed);
+    rightMotor.set(ControlMode.PercentOutput, speed);
   }
 
   public void setLeft(double speed) {
-    leftMotor.set(speed);
+    leftMotor.set(ControlMode.PercentOutput, speed);
   }
 
 
