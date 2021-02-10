d2b54bac02bed06b49a25280ee8bf0420e22a3a7
FredProbst <github@fsd.probst.org>
2021-02-09 19:57:43 -0500

    finishes up the TurnAngle command
    This commit finishes up the TurnAngle command by adding logic to the
    isFinished command and wrapping of the angle value. We add extra variables to
    support these changes.

--- src/main/java/frc/robot/commands/Drive/TurnAngle.java
+++ src/main/java/frc/robot/commands/Drive/TurnAngle.java
@@ -12,15 +12,24 @@ public class TurnAngle extends CommandBase {
   static Drive m_drive;
   // angle is in degrees
   double m_angle;
+  double m_finalAngle;
   boolean m_parameterized;
   double m_speed;
   double m_initialAngle;
+  // positive is clockwise, negative is counter-clockwise
+  double m_direction;
+
+  final double CLOCKWISE = 1;
+  final double COUNTER_CLOCKWISE = -1;
 
   /** Creates a new TurnAngle. */
   public TurnAngle(double angle, boolean parameterized) {
     m_drive = Drive.getInstance();
     m_parameterized = parameterized;
-    m_angle = angle;
+    m_direction = angle/Math.abs(angle);
+    m_angle = (Math.abs(angle) % 360) * m_direction;
+    updateFinalAngle();
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_drive);
   }
@@ -47,6 +56,7 @@ public class TurnAngle extends CommandBase {
       m_angle = TestingDashboard.getInstance().getNumber(m_drive, "AngleToTurnInDegrees");
       m_speed = TestingDashboard.getInstance().getNumber(m_drive, "SpeedWhenTurning");
     }
+    m_drive.tankDrive(m_speed * m_direction, m_speed * m_direction * -1);
   }
 
   // Called once the command ends or is interrupted.
@@ -56,6 +66,30 @@ public class TurnAngle extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    boolean finished = false;
+    if (m_direction == CLOCKWISE) {
+      if (m_drive.getYaw() > m_finalAngle) {
+        finished = true;
+      }
+    } else if (m_direction == COUNTER_CLOCKWISE) {
+      if (m_drive.getYaw() < m_finalAngle) {
+        finished = true;
+      }
+    }
+
+    return finished;
+  }
+
+  public void updateFinalAngle() {
+
+    m_finalAngle = m_initialAngle + m_angle;
+    if (m_finalAngle > 180) {
+      double delta = m_angle - (180-m_initialAngle);
+      m_finalAngle = -180 + delta;
+    } else if (m_finalAngle < -180) {
+      double delta = m_angle - (-180-m_initialAngle);
+      m_finalAngle = 180 + delta;
+    }
+
   }
 }
