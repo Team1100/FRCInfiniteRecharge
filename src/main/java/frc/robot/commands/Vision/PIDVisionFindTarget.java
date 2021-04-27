ddfb99db18eab371fb8e33ad3586813efaf78a4a
FredProbst <github@fsd.probst.org>
2021-04-27 19:31:00 -0400

    adds functionality to the PIDVisionFindTarget
    This commit finishes off the PIDVisionFindTarget command. The
    PIDVisionFindTarget command is the second step to center the
    turret on the target. It turns the robot until the target is
    within a certain offset.

--- src/main/java/frc/robot/commands/Vision/PIDVisionFindTarget.java
+++ src/main/java/frc/robot/commands/Vision/PIDVisionFindTarget.java
@@ -7,18 +7,22 @@ package frc.robot.commands.Vision;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.TestingDashboard;
+import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Vision;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class PIDVisionFindTarget extends PIDCommand {
-  static final double OFFSET_TOLERANCE = 100;
+  // the width of the camera stream is 480 pixels
+  static final double OFFSET_TOLERANCE = 100; // measured in pixels 
+  static final double MAX_SPEED = 1;
+  static Drive m_drive;
   /** Creates a new PIDVisionTurnToTarget. */
   public PIDVisionFindTarget() {
     super(
         // The controller that the command will use
-        new PIDController(0, 0, 0),
+        new PIDController(0.01, 0.01, 0.0),
         // This should return the measurement
         () -> Vision.getInstance().getTargetOffset(),
         // This should return the setpoint (can also be a constant)
@@ -26,9 +30,20 @@ public class PIDVisionFindTarget extends PIDCommand {
         // This uses the output
         output -> {
           // Use the output here
+          if (output > MAX_SPEED) {
+            output = MAX_SPEED;
+          }
+          if (output < -MAX_SPEED) {
+            output = -MAX_SPEED;
+          }
+            
+          m_drive.tankDrive(output, -output);
         });
     // Use addRequirements() here to declare subsystem dependencies.
     // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(OFFSET_TOLERANCE);
+    m_drive = Drive.getInstance();
+
   }
   public static void registerWithTestingDashboard() {
     Vision vision = Vision.getInstance();
@@ -41,9 +56,9 @@ public class PIDVisionFindTarget extends PIDCommand {
   @Override
   public boolean isFinished() {
     boolean finished = false;
-    if (!Vision.getInstance().isTargetFound())
+    if (!Vision.getInstance().isTargetFound()) // if the target is not found, the command is finished
       finished = true;
-    if (Math.abs(Vision.getInstance().getTargetOffset()) < OFFSET_TOLERANCE)
+    if (getController().atSetpoint())
       finished = true;
     return finished;
   }
