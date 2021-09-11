ecbf2c640883f7fe4921de05aae40ccab29e18ae
FredProbst <github@fsd.probst.org>
2021-09-10 20:45:20 -0400

    updates the vision program and changes shuffleboard box sizes
    This commit updates the vision program and also changes the shuffleboard
    box sizes to be more compatable with the 32 grid size on shuffleboard.

--- src/main/vision/uploaded.py
+++ src/main/vision/uploaded.py
@@ -14,16 +14,16 @@ imgResult = None
 mask = None
 team = None
 
-hueMin = 1
+hueMin = 13
 hueMax = 255
 satMin = 1
-satMax = 255
-valMin = 1
+satMax = 66
+valMin = 235
 valMax = 255
 
 boundingCenterX = 0
-boundingCenterY = 0
-
+centerY = 0
+centerX = 0
 targetDetected = False
 
 
@@ -45,10 +45,10 @@ def findColor(img,myColors):
 def getContours(img):
     image, contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    tolerance = .05
+    tolerance = .2
     idealRatio = 0.1 # this is the ideal ratio for the area ratio value. 
-    idealAspectRatio = 2.31 # this is the ideal aspect ratio based off of the diagram but can be changed as needed.
-    aspectTolerance = .5 # this is the tolerance for finding the target with the right aspect ratio
+    idealAspectRatio = 2 # this is the ideal aspect ratio based off of the diagram but can be changed as needed.
+    aspectTolerance = .44 # this is the tolerance for finding the target with the right aspect ratio
                          # start off with a large tolerance, and if the ideal ratio is correct, lower the tolerance as needed. 
     global garea
     global areaRatio
@@ -76,6 +76,8 @@ def getContours(img):
                     area = boundingArea
                     largestAreaRatio = areaRatio
                     largestAspectRatio = aspectRatio
+                else:
+                    print(str(aspectRatio))
 
         
         
@@ -85,6 +87,8 @@ def getContours(img):
             targetDetected = True
             global boundingCenterX #center of the bounding box x axis
             global boundingCenterY # center of the bounding box y axis
+            global centerX
+            global centerY
             cv2.drawContours(imgResult,largest, -1, (255,0,0), 3)
             peri = cv2.arcLength(largest, True)
             garea = area
@@ -92,8 +96,8 @@ def getContours(img):
             approx = cv2.approxPolyDP(largest, 0.02 * peri, True)
             x, y, w, h, = cv2.boundingRect(approx)
             boundingCenterX = x + w/2
-            #boundingCenterX = ((x + (w/2))-320)/320
-            boundingCenterY = ((y + (h/2))-240)/240
+            centerX = ((x + (w/2))-320)/320
+            centerY = ((y + (h/2))-240)/240
 
             cv2.rectangle(imgResult, (x,y),(x+w,y+h),(0,255,0),3)
 
@@ -124,11 +128,11 @@ def main():
    #ntinst.startClient(ip)
    vision_nt = ntinst.getTable('Shuffleboard/Vision')
 
-   vision_nt.putNumber('hueMin',1)
+   vision_nt.putNumber('hueMin',13)
    vision_nt.putNumber('hueMax',255)
    vision_nt.putNumber('satMin',1)
-   vision_nt.putNumber('satMax',255)
-   vision_nt.putNumber('valMin',1)
+   vision_nt.putNumber('satMax',66)
+   vision_nt.putNumber('valMin',235)
    vision_nt.putNumber('valMax',255)
 
 
@@ -148,6 +152,8 @@ def main():
 
         global boundingCenterX
         global boundingCenterY
+        global centerX
+        global centerY
         global imgResult
         global mask
         global hueMin
@@ -190,7 +196,7 @@ def main():
         else: # only sets updates the targetDetected if a certain amount of time has passed
             if timeDiff > targetDetTol:
                 vision_nt.putNumber('targetDetected',0)
-           
+         
         count +=1
         loopLen = 25
         vision_nt.putNumber('realTimeArea',garea) # these lines put all the necessary data on network tables, which are then displayed on shuffleboard
@@ -201,8 +207,9 @@ def main():
         vision_nt.putNumber('aspectRatio', aspectRatio)
         
         vision_nt.putNumber('largestAspectRatio', largestAspectRatio)
-        vision_nt.putNumber('CenterOfBoxX', boundingCenterX)
-        vision_nt.putNumber('CenterOfBoxY', boundingCenterY)
+        vision_nt.putNumber('CenterOfBoxX', centerX)
+        vision_nt.putNumber('CenterOfBoxY', centerY)
+
         if sumArea > 0 and count >= loopLen:
         #if count >= loopLen:
             average = sumArea/count
