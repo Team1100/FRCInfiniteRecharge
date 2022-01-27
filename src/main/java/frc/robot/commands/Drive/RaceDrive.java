2df4a9f882a964ee63736477dc1c2ea95a740aa8
Thor Smith <thorsmith777@gmail.com>
2022-01-26 21:09:47 -0500

    Added RaceDrive Class

---
+++ src/main/java/frc/robot/commands/Drive/RaceDrive.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Drive;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.OI;
+import frc.robot.TestingDashboard;
+import frc.robot.input.XboxController;
+import frc.robot.input.XboxController.XboxAxis;
+import frc.robot.subsystems.Drive;
+
+
+public class RaceDrive extends CommandBase {
+  /** Creates a new RaceDrive. */
+  private final Drive m_drive;
+  private static OI oi;
+  private static XboxController m_xbox;
+  
+
+  public RaceDrive() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drive = Drive.getInstance();
+    
+    addRequirements(m_drive);
+    
+  }
+
+  public static void registerWithTestingDashboard() {
+    Drive drive = Drive.getInstance();
+    RaceDrive cmd = new RaceDrive();
+    TestingDashboard.getInstance().registerCommand(drive, "Basic", cmd);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    oi = OI.getInstance();
+    m_xbox = oi.getXbox();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    /*if forward and reverse are not "0", then disable movement
+    
+    */
+
+    double rotation = m_xbox.getAxis(XboxAxis.kXRight);
+    double forward = m_xbox.getAxis(XboxAxis.kLeftTrigger);
+    double reverse = m_xbox.getAxis(XboxAxis.kRightTrigger);
+
+    m_drive.raceDrive(forward, rotation);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
